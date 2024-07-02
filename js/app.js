f4f46589e836222d8138830d7c0ad1b688db9f7f
Anne-M <77175884+annemarieb78@users.noreply.github.com>
2024-07-02 10:20:41 +0200

    Updates

---
+++ js/app.js
@@ -0,0 +1,6 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import "./styles.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
